ad083bb4b10b666af7492b35b852f7e80fc7980c
kriss <krzysztof.podmokly@gmail.com>
2025-01-08 17:47:59 +0100

    Skateboard builder

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import React from "react";
 import { CustomizerControlsProvider } from "./context";
 import { createClient } from "@/prismicio";
+import Preview from "./Preview";
+import { asImageSrc } from "@prismicio/client";
+import Controls from "./Controls";
 
 const page = async () => {
   const client = createClient();
@@ -16,6 +19,13 @@ const page = async () => {
   const defaultTruck = metals[0];
   const defaultBolt = metals[0];
 
+  const wheelTextureURLs = wheels
+    .map((texture) => asImageSrc(texture.texture))
+    .filter((url): url is string => Boolean(url));
+  const deckTextureURLs = decks
+    .map((texture) => asImageSrc(texture.texture))
+    .filter((url): url is string => Boolean(url));
+
   return (
     <div className="flex min-h-screen flex-col lg:flex-row">
       <CustomizerControlsProvider
@@ -25,6 +35,13 @@ const page = async () => {
         defaultBolt={defaultBolt}
       >
         <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
+          <div className="absolute inset-0">
+            <Preview
+              deckTextureURLs={deckTextureURLs}
+              wheelTextureURLs={wheelTextureURLs}
+            />
+          </div>
+
           <Link href="/" className="absolute left-6 top-6">
             <Logo className="h-12 text-white" />
           </Link>
@@ -33,6 +50,13 @@ const page = async () => {
           <Heading as="h1" size="sm" className="mb-6 mt-0">
             Build your board
           </Heading>
+
+          <Controls
+            wheels={wheels}
+            decks={decks}
+            metals={metals}
+            className="mb-6"
+          />
 
           <ButtonLink href="" color="lime" icon="plus">
             Add to cart
